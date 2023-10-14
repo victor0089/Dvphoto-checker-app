4acb31b1a628b7c83293c1f336697fb48843b271
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-10-14 23:37:27 +0300

    Create check-background-uniform.py
    In this code, we calculate the standard deviation of pixel values in the grayscale image. A low standard deviation indicates a relatively uniform background because the pixel values are similar. You can adjust the uniformity_threshold value to control the threshold for background uniformity.
    
    Please note that this is a simplified example, and the actual DV Lottery requirements for background uniformity may be more specific. You should consult the official DV Lottery guidelines and adapt the code accordingly.

---
+++ check-background-uniform.py
@@ -0,0 +1,31 @@
+# pip install opencv-python #
+
+import cv2
+import numpy as np
+
+def verify_photo(photo_path):
+    # Load the captured photo
+    photo = cv2.imread(photo_path)
+
+    # Convert the image to grayscale for background analysis
+    grayscale = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+
+    # Calculate the standard deviation of pixel values in the grayscale image
+    std_dev = np.std(grayscale)
+
+    # Define a threshold for background uniformity
+    uniformity_threshold = 10  # Adjust as needed
+
+    # Check if the standard deviation is below the threshold, indicating a uniform background
+    if std_dev < uniformity_threshold:
+        return True  # Photo meets the requirements
+
+    return False  # Photo does not meet the requirements
+
+# Example usage
+photo_path = "captured_photo.png"
+result = verify_photo(photo_path)
+if result:
+    print("Photo meets DV Lottery requirements.")
+else:
+    print("Photo does not meet requirements.")
